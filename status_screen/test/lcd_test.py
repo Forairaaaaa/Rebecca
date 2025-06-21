be335698bd81fead6171e057765a20a6e74a05d7
Forairaaaaa <applesyqd@outlook.com>
2025-06-21 19:54:49 +0100

    better test

--- status_screen/test/lcd_test.py
+++ status_screen/test/lcd_test.py
@@ -9,8 +9,8 @@ import random
 class st7789_with_offset(st7789):
     def __init__(self, serial_interface=None, width=240, height=240, rotate=0, offset=(0, 0), **kwargs):
         self._offset = offset
-        super().__init__(serial_interface=serial_interface, width=width,
-                         height=height, rotate=rotate, **kwargs)
+        super().__init__(serial_interface=serial_interface,
+                         width=width, height=height, rotate=rotate, **kwargs)
 
     def display(self, image):
         self.set_window(self._offset[0], self._offset[1],
@@ -28,29 +28,46 @@ def main():
 
     with canvas(device) as draw:
         draw.rectangle(device.bounding_box, outline="white", fill="black")
-        draw.text((0, 40), "Hello World", fill="red", font_size=48)
+        draw.text((23, 167), "fuck that shit", fill="pink", font_size=36)
+    time.sleep(1)
 
-    # while True:
-    #     time.sleep(1)
+    def random_color(): return random.choice(
+        ["red", "green", "blue", "yellow", "purple", "orange", "pink", "brown", "gray", "black", "white"])
 
-    # bl_list = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    # while True:
-    #     for bl in bl_list:
-    #         device.backlight(bl)
-    #         print(f"Backlight: {bl}")
-    #         time.sleep(0.5)
+    def random_point(): return (random.randint(-123, 280), random.randint(-123, 240))
 
-    device.backlight(100)
     while True:
-        with canvas(device) as draw:
-            for i in range(6):
-                x0 = random.randint(0, 280)
-                y0 = random.randint(0, 240)
-                x1 = random.randint(x0, 280)
-                y1 = random.randint(y0, 240)
-                draw.rectangle((x0, y0, x1, y1), outline="white", fill=random.choice(
-                    ["red", "green", "blue", "yellow", "purple", "orange", "pink", "brown", "gray", "black", "white"]))
-        # time.sleep(0.2)
+        for i in range(2):
+            for bl in range(100, 0, -2):
+                device.backlight(bl)
+                print(f"bl: {bl}")
+                time.sleep(0.01)
+            for bl in range(0, 100, 2):
+                device.backlight(bl)
+                print(f"bl: {bl}")
+                time.sleep(0.01)
+
+        print("keep render shit")
+        device.backlight(100)
+        for i in range(123):
+            with canvas(device) as draw:
+                for i in range(3):
+
+                    draw.circle(random_point(), random.randint(
+                        1, 66), fill=random_color())
+
+                    x0, y0 = random_point()
+                    x1, y1 = random.randint(x0, 321), random.randint(y0, 321)
+                    draw.rectangle((x0, y0, x1, y1),
+                                   outline=random_color(), fill=random_color())
+
+                    # line
+                    draw.line((random_point(), random_point()),
+                              fill=random_color(), width=random.randint(1, 12))
+
+                    for i in range(6):
+                        draw.text(random_point(), "fuck",
+                                  fill=random_color(), font_size=18)
 
 
 if __name__ == "__main__":
